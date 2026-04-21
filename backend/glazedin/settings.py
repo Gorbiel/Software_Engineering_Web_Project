d6ade64e1a96596b12d73389b7b99c7ecdf5dad8
Gabriel Kania <gracz1212yt@gmail.com>
2026-04-21 15:52:41 +0200

    Initialized Next.JS

--- backend/glazedin/settings.py
+++ backend/glazedin/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'glazedin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'backend/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
