c630dbe526a350052bc1c99565c7a2d0fde31118
Gabriel Kania <gracz1212yt@gmail.com>
2026-05-04 21:40:51 +0200

    IO26-40 tests fix, hopefully

---
+++ backend/config/settings/test.py
@@ -0,0 +1,14 @@
+from .dev import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test.sqlite3",
+    }
+}
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.MD5PasswordHasher",
+]
+
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
