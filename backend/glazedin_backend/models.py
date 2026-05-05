21ca3e804151551d28091ba9d416f3560848d0c4
MinerOfSouls <58173484+MinerOfSouls@users.noreply.github.com>
2026-05-05 12:09:39 +0200

    Added checks

--- backend/glazedin_backend/models.py
+++ backend/glazedin_backend/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.core import validators
+from django.views.decorators.http import condition
+
 
 # Create your models here.
 
@@ -12,11 +15,25 @@ class User(models.Model):
     deactivation_date - set this field when deactivating the user
     """
     name = models.CharField(max_length=64)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     creation_date = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
-    deactivation_date = models.DateTimeField(default=None) #TODO: Może się da o jakoś triggerem zrobić
+    deactivation_date = models.DateTimeField(default=None) #TODO: create migration trigger to set automaticaly
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="deleted_after_created",
+                condition=models.Q(creation_date__lt=models.F("deactivation_date")) |
+                          models.Q(deactivation_date__isnull=True)
+            ),
+            models.CheckConstraint(
+                name="inactive_when_deactivation_date_is_not_null",
+                condition=(models.Q(active__exact=False) & models.Q(deactivation_date__isnull=False)) |
+                          (models.Q(active__exact=True) & models.Q(deactivation_date__isnull=True))
+            )
+        ]
 
 class Achievement(models.Model):
     """
@@ -37,12 +54,20 @@ class Glaze(models.Model):
     body = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                name="user_cant_glaze_themself",
+                condition=~models.Q(posting_user_id__exact=models.F('receiving_user_id'))
+            )
+        ]
+
 class AchievementConfirmation(models.Model):
     """
     User confirming someone else's achievement
     """
     achievement_id = models.ForeignKey(Achievement)
-    user_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(User) #TODO: create migration trigger so the user cant confirm their own achievement
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class ConfirmationRequest(models.Model):
@@ -50,14 +75,14 @@ class ConfirmationRequest(models.Model):
     User send this to a second user to confirm their achievement
     """
     achievement_id = models.ForeignKey(Achievement)
-    receiving_user_id = models.ForeignKey(User)
+    receiving_user_id = models.ForeignKey(User) # TODO: create migration trigger so the user cant send a confirmtion to themself
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class Team(models.Model):
     """
     Team declaration
     """
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class TeamLeader(models.Model):
@@ -75,7 +100,7 @@ class TeamMember(models.Model):
     """
     team_id = models.ForeignKey(Team)
     user_id = models.ForeignKey(User)
-    rank = models.IntegerField(default=1)
+    rank = models.IntegerField(default=1, validators=[validators.MinValueValidator(0)])
 
 class Admin(models.Model):
     """
@@ -87,7 +112,7 @@ class Tag(models.Model):
     """
     Defines an achievent/galze tag
     """
-    tag_text = models.CharField(max_length=64)
+    tag_text = models.CharField(max_length=64, unique=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class AchievementTag(models.Model):
@@ -102,8 +127,8 @@ class GlazeTag(models.Model):
     tag_id = models.ForeignKey(Tag)
 
 class Reaction(models.Model):
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
+    code = models.CharField(max_length=64, unique=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class AchievementReaction(models.Model):
