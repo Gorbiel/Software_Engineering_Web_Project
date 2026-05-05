0c33eb2402a10fa8848ad3c64c17e74be035f4d2
MinerOfSouls <58173484+MinerOfSouls@users.noreply.github.com>
2026-05-05 18:02:41 +0200

    migration, triggers, validation
    Migrated model to db
    Added nesseary validation and automation triggers
    added data validation and on delete behaviour

--- backend/core/models.py
+++ backend/core/models.py
@@ -11,15 +11,16 @@ class User(models.Model):
     """
     Normal user
     name - combined name and surname / username
+    creation_date - set automatically
     active - sets the user as active, defaults to True
-    deactivation_date - set this field when deactivating the user
+    deactivation_date - set automatically
     """
     name = models.CharField(max_length=64)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     creation_date = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
-    deactivation_date = models.DateTimeField(default=None) #TODO: create migration trigger to set automaticaly
+    deactivation_date = models.DateTimeField(default=None, blank=True, null=True)
 
     class Meta:
         constraints = [
@@ -39,7 +40,7 @@ class Achievement(models.Model):
     """
     A users achievement
     """
-    user_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.TextField()
     body = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -48,8 +49,8 @@ class Glaze(models.Model):
     """
     A shout-out from one user to another
     """
-    posting_user_id = models.ForeignKey(User)
-    receiving_user_id = models.ForeignKey(User)
+    posting_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    receiving_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
     title = models.TextField()
     body = models.TextField()
     creation_date = models.DateTimeField(auto_now_add=True)
@@ -66,16 +67,16 @@ class AchievementConfirmation(models.Model):
     """
     User confirming someone else's achievement
     """
-    achievement_id = models.ForeignKey(Achievement)
-    user_id = models.ForeignKey(User) #TODO: create migration trigger so the user cant confirm their own achievement
+    achievement_id = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE) #TODO: create migration trigger so the user cant confirm their own achievement
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class ConfirmationRequest(models.Model):
     """
     User send this to a second user to confirm their achievement
     """
-    achievement_id = models.ForeignKey(Achievement)
-    receiving_user_id = models.ForeignKey(User) # TODO: create migration trigger so the user cant send a confirmtion to themself
+    achievement_id = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    receiving_user_id = models.ForeignKey(User, on_delete=models.CASCADE) # TODO: create migration trigger so the user cant send a confirmtion to themself
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class Team(models.Model):
@@ -90,23 +91,23 @@ class TeamLeader(models.Model):
     Defines a team leader for a team, a user can be a team leader of multiple teams
     and a team can have multiple leaders.
     """
-    team_id = models.ForeignKey(Team)
-    user_id = models.ForeignKey(User)
+    team_id = models.ForeignKey(Team, on_delete=models.RESTRICT)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class TeamMember(models.Model):
     """
     Defines a member of a team and their rank in the team.
     Ranks shouldn't be compared between teams.
     """
-    team_id = models.ForeignKey(Team)
-    user_id = models.ForeignKey(User)
+    team_id = models.ForeignKey(Team, on_delete=models.RESTRICT)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     rank = models.IntegerField(default=1, validators=[validators.MinValueValidator(0)])
 
 class Admin(models.Model):
     """
     Defines a user with administrative privileges in the app.
     """
-    user_id = models.ForeignKey(User)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Tag(models.Model):
     """
@@ -119,12 +120,12 @@ class AchievementTag(models.Model):
     """
     Achievements tagged with specific tags
     """
-    achievement_id = models.ForeignKey(Achievement)
-    tag_id = models.ForeignKey(Tag)
+    achievement_id = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 class GlazeTag(models.Model):
-    glaze_id = models.ForeignKey(Glaze)
-    tag_id = models.ForeignKey(Tag)
+    glaze_id = models.ForeignKey(Glaze, on_delete=models.CASCADE)
+    tag_id = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
 class Reaction(models.Model):
     name = models.CharField(max_length=64, unique=True)
@@ -132,11 +133,13 @@ class Reaction(models.Model):
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class AchievementReaction(models.Model):
-    achievement_id = models.ForeignKey(Achievement)
-    reaction_id = models.ForeignKey(Reaction)
+    achievement_id = models.ForeignKey(Achievement, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    reaction_id = models.ForeignKey(Reaction, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
 
 class GlazeReaction(models.Model):
-    glaze_id = models.ForeignKey(Glaze)
-    reaction_id = models.ForeignKey(Reaction)
+    glaze_id = models.ForeignKey(Glaze, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    reaction_id = models.ForeignKey(Reaction, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
