e03725d3ad8d66c7b0fd73ac7a7c4cc1e509c009
MinerOfSouls <58173484+MinerOfSouls@users.noreply.github.com>
2026-05-19 22:32:07 +0200

    Added missing queries

--- backend/apps/achievements/models.py
+++ backend/apps/achievements/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 from django.db.models import Count
 
+from apps.teams.models import TeamMember
 from apps.users.models import User
+
+
+class AchievementConfirmationQuerySet(models.QuerySet):
+
+    def with_weighted_score_for_user(self, user):
+        user_teams = TeamMember.objects.filter(user_id=user).values_list(
+            "team_id", flat=True
+        )
+
+        confirmer_avg_rank_in_shared_teams = (
+            TeamMember.objects.filter(
+                user_id=models.OuterRef("user_id"), team_id__in=user_teams
+            )
+            .values("user_id")
+            .annotate(avg_rank=models.Avg("rank"))
+            .values("avg_rank")
+        )
+
+        return self.annotate(
+            rank=models.Case(
+                models.When(
+                    user_id__teammember__team_id__in=user_teams,
+                    then=models.Subquery(confirmer_avg_rank_in_shared_teams[:1]),
+                ),
+                default=models.Value(1),
+                output_field=models.IntegerField(),
+            )
+        )
 
 
 class AchievementQuerySet(models.QuerySet):
@@ -22,6 +51,15 @@ class AchievementQuerySet(models.QuerySet):
 
     def with_reaction_count(self):
         return self.annotate(reaction_count=Count("achievementreaction"))
+
+    def with_weighted_confirmation_score(self, user):
+        return self.annotate(
+            confirmation_score=(
+                AchievementConfirmation.confirmations.with_weighted_score_for_user(user)
+                .filter(achievement_id="id")
+                .aggregate(models.Sum("rank"))
+            )
+        )
 
 
 class Achievement(models.Model):
@@ -47,6 +85,9 @@ class AchievementConfirmation(models.Model):
     achievement = models.ForeignKey(Achievement, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     creation_date = models.DateTimeField(auto_now_add=True)
+
+    objects = models.Manager()
+    confirmations = AchievementConfirmationQuerySet.as_manager()
 
 
 class ConfirmationRequest(models.Model):
