40d412338dea14d2337a6afda6dd3979d474e0cb
Gabriel Kania <gracz1212yt@gmail.com>
2026-06-04 22:32:17 +0200

    feature: add achievement search

--- backend/apps/achievements/urls.py
+++ backend/apps/achievements/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from apps.achievements.views import AchievementViewSet
+from apps.achievements.views import AchievementSearchViewSet, AchievementViewSet
 
 router = DefaultRouter()
+router.register("search", AchievementSearchViewSet, basename="achievement-search")
 router.register("", AchievementViewSet, basename="achievements")
 
 urlpatterns = router.urls
