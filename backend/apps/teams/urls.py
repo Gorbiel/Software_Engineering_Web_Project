c44fdf24e30f721b984bba1225497d12be6b82b4
Gabriel Kania <42325931+Gorbiel@users.noreply.github.com>
2026-06-16 11:29:00 +0200

    feature: add team management (#100)

--- backend/apps/teams/urls.py
+++ backend/apps/teams/urls.py
@@ -4,6 +4,7 @@ from apps.teams.views import TeamSearchViewSet, TeamViewSet
 
 router = DefaultRouter()
 router.register("search", TeamSearchViewSet, basename="team-search")
-router.register("team", TeamViewSet, basename="team")
+router.register("team", TeamViewSet, basename="team-legacy")
+router.register("", TeamViewSet, basename="team")
 
 urlpatterns = router.urls
